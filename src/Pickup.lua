88bd4feaa12a2dd82acdd342a19c61d77e04b7c6
muzza74 <muzza74@gmail.com>
2015-10-02 21:42:53 +1000

    pickup lined up with collider FOR NOW

--- src/Pickup.lua
+++ src/Pickup.lua
@@ -7,9 +7,9 @@ local TexMateStatic = require("texmate.TexMateStatic")
 Pickup:include(require('stateful'))
 
 function Pickup:initialize()
-  self.frame = TexMateStatic(PROTOTYPEASSETS,"rockDirt.png")
+  self.frame = TexMateStatic(PROTOTYPEASSETS,"rockDirt.png",0,0,65,123)
   self.collision = world:newCircleCollider(300, 300, 50)
-  self.collision.body:setFixedRotation(false)
+  self.collision.body:setFixedRotation(true)
   self.collision.fixtures['main']:setRestitution(0.3)
   self.collision.body:applyLinearImpulse(100,0,self.collision.body:getX()-30,self.collision.body:getY()-30)
 end
