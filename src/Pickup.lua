5566444cb1f5943104b4f7548408ab4eef2537ea
muzza74 <muzza74@gmail.com>
2015-10-02 20:20:24 +1000

    preppin pickup physics for testing in game scene

--- src/Pickup.lua
+++ src/Pickup.lua
@@ -3,18 +3,24 @@ local Pools = require("src.effectsPool")
 local TexMate = require("texmate.TexMate")
 local world = require('src.world')
 local Pickup = class('Pickup')
-local TexMateStatic = require('src.TexMateStatic')
+local TexMateStatic = require("texmate.TexMateStatic")
 Pickup:include(require('stateful'))
 
 function Pickup:initialize()
-  --frame = TexMateStatic:new("assets/entities/rockDirt.png",1,9,3,)}
+  self.frame = TexMateStatic(PROTOTYPEASSETS,"rockDirt.png")
+  self.collision = world:newRectangleCollider(300, 300, 50, 50, {collision_class = 'Player'})
+  self.collision.body:setFixedRotation(false)
+  self.collision.fixtures['main']:setRestitution(0.3)
+  self.collision.body:applyLinearImpulse(100,0,self.collision.body:getX()-30,self.collision.body:getY()-30)
 end
 
 function Pickup:update(dt)
 
+  self.collision.body:applyLinearImpulse(500, 0)
 end
 
 function Pickup:draw()
-
+  self.frame:draw()
 end
 
+return Pickup
