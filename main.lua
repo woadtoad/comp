2597caa49172176959e93d90e64da6e5ed3fa5fa
Muzz <muzzoid@gmail.com>
2015-10-02 19:21:40 +1000

    Added new atlas

--- main.lua
+++ main.lua
@@ -35,6 +35,7 @@ class = require("middleclass")
 -- Following are suited to being globals
 UI = require("thranduil.ui")
 ATLAS = require("texmate.AtlasImporter").loadAtlasTexturePacker( "assets.entities.entitiesC", "assets/entities/entitiesC.png" )
+PROTOTYPEASSETS = require("texmate.AtlasImporter").loadAtlasTexturePacker( "assets.entities.TileAssets", "assets/entities/TileAssets.png" )
 
 -- Setup our UI framework with a offshelf theme
 UI.DefaultTheme = require("thranduil.Theme")
